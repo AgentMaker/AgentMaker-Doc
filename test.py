99df76d973ffd0723ada2cfa229805255a007fc4
GT-Zhang <46156734+GT-ZhangAcer@users.noreply.github.com>
2021-04-27 14:39:49 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import time
+
+def foo(x: int, y=10):
+    """
+    Add
+    :param x: 
+    :param y: 
+    :return: result
+    
+    Example:
+    print(foo(1))
+    """
+    return x + y
+
+
+if __name__ == '__main__':
+    print(foo(1))
